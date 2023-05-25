413622eb10231d6d34880d5ec1a7f882228fc1a2
Bibiana Sebestianova <b.sebestianova@gmail.com>
2023-05-25 14:47:58 +0200

    feat: adding example package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,17 @@
+'use client';
+
 import { Button, Header } from "ui";
+import { add } from "@bibiana/utilities";
 
 export default function Page() {
+  const addition = ()  =>{
+    console.log(add(2,2))
+  }
+
   return (
     <>
       <Header text="Web" />
+       <button onClick={addition}>Add</button>
       <Button />
     </>
   );
