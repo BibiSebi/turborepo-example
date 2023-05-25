3fb8dd7ca271c132037d806badbfb3804aeb3c7d
Bibiana Sebestianova <b.sebestianova@gmail.com>
2023-05-25 15:34:29 +0200

    feat: changeset patch version

--- packages/utilities/CHANGELOG.md
+++ packages/utilities/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @bibiana/utilities
 
+## 2.0.1
+
+### Patch Changes
+
+- Added square function
+
 ## 2.0.0
 
 ### Major Changes
--- packages/utilities/index.ts
+++ packages/utilities/index.ts
@@ -1,3 +1,6 @@
 
 type Add = (a:number, b:number) => number
+type Square = (a:number) => number
 export const add: Add = (a, b) =>  a+b
+
+export const square: Square = (a) => a*a
